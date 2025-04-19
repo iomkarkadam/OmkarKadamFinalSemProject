6fde79c307ef41137e0e2787f6e70291fa568796
iomkarkadam <omkarsk.0101@gmail.com>
2025-04-19 14:37:04 +0530

    admin app Source code added

---
+++ resiliency-admin-app/resiliency-admin-app/src/main/java/com/bits/resiliency_admin_app/ResiliencyAdminAppApplication.java
@@ -0,0 +1,13 @@
+package com.bits.resiliency_admin_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResiliencyAdminAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResiliencyAdminAppApplication.class, args);
+	}
+
+}
